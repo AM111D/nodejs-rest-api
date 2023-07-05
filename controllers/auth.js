8fc66715dbbcb29432ad4d1f044443dc65e0a2fd
AM111D <Dimasssnike@icloud.com>
2023-07-05 02:17:33 -0700

    change_httpError

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,21 +1,24 @@
 const { User } = require("../models/users");
+const { httpError } = require("../helpers");
 
-const httpError = require("../helpers/httpError");
-
-const register = async (req, res) => {
+const register = async (req, res, next) => {
   const { email } = req.body;
-  const user = await User.findOne({ email });
+  try {
+    const user = await User.findOne({ email });
 
-  if (user) {
-    throw httpError(409, "Email already in use");
+    if (user) {
+      throw httpError(409, "Email already in use");
+    }
+
+    const newUser = await User.create(req.body);
+
+    res.status(201).json({
+      email: newUser.email,
+      name: newUser.name,
+    });
+  } catch (error) {
+    next(error); // Передаем ошибку в следующий обработчик ошибок
   }
-
-  const newUser = await User.create(req.body);
-
-  res.status(201).json({
-    email: newUser.email,
-    name: newUser.name,
-  });
 };
 
 module.exports = {
--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,7 @@
-// const httpError = require("./httpError");
+const httpError = require("./httpError");
 const handleMongooseError = require("./handleMongooseError");
 
 module.exports = {
-  // httpError,
+  httpError,
   handleMongooseError,
 };
